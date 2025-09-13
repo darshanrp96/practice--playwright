5c818e8c67a84796a7585ed2fe4d6c0c7561b399
darshan <darshanpardeshi8@gmail.co>
2025-09-13 20:49:37 +0530

    second test chnges added

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -1,2 +1,15 @@
 console.log("second test");
 
+const {test,expect} = require('@playwright/test')
+
+test("Verify title", async ({page}) => {
+      await page.goto("https://www.google.com/")
+
+      const url = await page.url()
+        console.log("url is "+url);
+      const title= await page.title()
+       console.log("url is "+title);
+
+      await expect(page).toHaveTitle("Google")}
+
+)
