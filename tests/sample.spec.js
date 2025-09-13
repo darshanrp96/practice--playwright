dc86124067098476a73fdd7a5abca34911122765
darshan <darshanpardeshi8@gmail.co>
2025-09-13 19:58:26 +0530

    alll test executed

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -4,3 +4,19 @@ test("my first test", async function ({page}) {
   //    
 })
 
+test("my second test", async function ({page}) {
+   expect(12).toBe(12)
+})
+
+test("my third test", async function ({page}) {
+   expect(100).toBe(100)
+})
+
+test("my fourth test", async function ({page}) {
+   expect("darshan pardeshi").toContain("pardeshi")  
+})
+
+test.skip("my fifth test", async function ({page}) {
+  //    
+})
+
