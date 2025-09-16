914d17fb131d5c9c563ececcac6aa910a8481b59
darshan <darshanpardeshi8@gmail.co>
2025-09-16 22:18:00 +0530

    login test passed

---
+++ tests/login.spec.js
@@ -0,0 +1,16 @@
+console.log("login test");
+const {test, expect} = require("@playwright/test");
+
+test("Validate login", async function({page}){
+
+await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+await page.fill("//input[@name='username']","Admin");
+await page.locator("//input[@name='password']").fill("admin123");
+await page.click("//button[@type='submit']");
+await page.waitForLoadState('networkidle');
+await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+
+})
+
+
+
