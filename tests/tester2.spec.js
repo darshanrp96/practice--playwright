f04b7a656ccf763afc6abd019aedc484754b609a
darshan <darshanpardeshi8@gmail.co>
2026-02-08 12:49:15 +0530

    new changes

--- tests/tester2.spec.js
+++ tests/tester2.spec.js
@@ -1,4 +1,5 @@
 import {test,expect} from "@playwright/test"
+import path from "path";
 
 test('Login test', async ({page}) => {
 
@@ -6,10 +7,18 @@ test('Login test', async ({page}) => {
 
 await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
-await page.getByPlaceholder('Username').fill('Admin');
-await page.getByPlaceholder('Password').fill('admin123');
+await page.getByPlaceholder('Username').fill("Admin");
+await page.getByPlaceholder('Password').fill("admin123");
 await page.getByRole('button', { name: 'Login' }).click();
 
 await expect(page).toHaveTitle('OrangeHRM');
+
+//logout
+
+await page.getByAltText("profile picture").click();
+await page.getByText("Logout").click();
+
+await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
 })
 
